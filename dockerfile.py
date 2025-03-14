05fc6338dc59c8089184df69e34408ddc949be95
artuuursliepins <artuuursliepins@gmail.com>
2025-03-14 23:57:31 +0200

    Update and rename dockerfile to dockerfile.py

--- dockerfile.py
+++ dockerfile.py
@@ -12,3 +12,15 @@ COPY . /app
 
 # Norādām noklusējuma komandu, lai serveris vienmēr paliktu dzīvs
 CMD ["tail", "-f", "/dev/null"]
+
+FROM ubuntu:20.04
+
+RUN apt-get update && apt-get install -y musescore3 xvfb python3 python3-pip
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip3 install -r requirements.txt
+
+CMD ["python3", "app.py"]
